e55561a8291fad925516249d6f50dfb0d4534da5
Michal Lekstan <m.lekstan@interia.pl>
2025-11-09 21:33:15 +0100

    Saving theme mode

--- src/providers/ThemeModeProvider.tsx
+++ src/providers/ThemeModeProvider.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from "@mui/material";
-import { useState, type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import { darkTheme, lightTheme } from "../theme";
 import { ThemeModeContext, type ThemeModes } from "../hooks/useThemeContext";
 
 export function ThemeModeProvider({ children }: { children: ReactNode }) {
-  const [mode, setMode] = useState<ThemeModes>("light");
+  const [mode, setMode] = useState<ThemeModes>(localStorage.getItem("themeMode") as "light" | "dark" || "light");
 
   const theme = (mode === "light") ? lightTheme : darkTheme;
+  
+  useEffect(() => {
+    localStorage.setItem("themeMode", mode);
+  }, [mode]);
+
 
   return (
     <ThemeModeContext.Provider 
